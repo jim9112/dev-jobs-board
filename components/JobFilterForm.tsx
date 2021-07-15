cea5fc88ff23ebc85e84032c6e98e1997dec3f1d
jim9112 <jim9112@gmail.com>
2021-07-15 17:41:52 -0400

    job filter form styles

--- components/JobFilterForm.tsx
+++ components/JobFilterForm.tsx
@@ -1,14 +1,38 @@
+import Image from 'next/image';
+import searchIcon from '../public/assets/desktop/icon-search.svg';
+import locationIcon from '../public/assets/desktop/icon-location.svg';
+
 const JobFilterForm = () => {
   return (
-    <div>
-      <form>
-        <input type="text" name="data" id="data" />
-        <input type="text" name="location" id="location" />
-        <input type="checkbox" name="fullTime" id="fullTime"></input>
-        <label htmlFor="fulltime">Full Time Only</label>{' '}
+    <>
+      <form className="w-full h-20 bg-secondary-white grid grid-cols-3 items-center">
+        <div>
+          <label htmlFor="data">
+            <Image src={searchIcon} alt="search icon" />{' '}
+          </label>
+          <input type="text" name="data" id="data" />
+        </div>
+        <div>
+          <label htmlFor="location">
+            <Image src={locationIcon} alt="location icon" />{' '}
+          </label>
+          <input type="text" name="location" id="location" />
+        </div>
+        <div>
+          <input type="checkbox" name="fullTime" id="fullTime"></input>
+          <label htmlFor="fulltime">Full Time Only</label>{' '}
+          <button
+            type="submit"
+            className="bg-primary-violet hover:bg-primary-light-violet text-secondary-white w-28 h-12 font-main font-bold rounded-md cursor-pointer"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log('submit');
+            }}>
+            Search
+          </button>
+        </div>
       </form>
-      <h1>Job Filter Form</h1>
-    </div>
+    </>
   );
 };
 
